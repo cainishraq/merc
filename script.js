485021b949181554fba01cacd1a2574d69df9434
Rakin Ishraq <holla@thatkidfrom.nyc>
2024-02-20 19:30:19 -0500

    added left/right buttons in modal

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+current = ["pushup",10]
 document.addEventListener('click', function(e) {
     e = e || window.event;
     var target = e.target || e.srcElement;
@@ -5,21 +6,39 @@ document.addEventListener('click', function(e) {
 		var exercise = target.parentNode.parentNode.parentNode.getAttribute("name").toLowerCase(),
 			index = target.cellIndex+1;
 		if (exercise == "fingertippushup") exercise = "pushup";
-		modal_open(exercise+index+".png");
+		exercises = exercise == "hang" ? 8 : 10
+		current = [exercise,index]; modal_open(index != 1, index != exercises);
 	} else if (target.nodeName == "A") {
-		modal_open("fingertip"+".png");
+		current = ["fingertip",'']; modal_open(false, false);
+	} else if (target.id == "left-arrow" || target.id == "right-arrow") {
+        if (target.id == "left-arrow") current[1] -= 1;
+        else if (target.id == "right-arrow") current[1] += 1;
+        modal_open(current[1] != 1, current[1] != exercises);
 	}
+	
 }, false);
 
 var modal = document.getElementById("modal");
 var modalImg = document.getElementById("modal-img");
 var captionText = document.getElementById("caption");
-var modal_open = function(fl){
+var modal_open = function(left, right) {
+	modalImg.src = "";
+    document.body.style.overflow = 'hidden';
+	var leftArrow = document.getElementById('left-arrow');
+	var rightArrow = document.getElementById('right-arrow');
+	var caption = document.getElementById('caption');
+	
+	leftArrow.style.display = left ? 'block' : 'none';
+	rightArrow.style.display = right ? 'block' : 'none';
+	console.log(left, right, leftArrow.style.display);
+
 	modal.style.display = "block";
-	modalImg.src = "images/"+fl;
+	modalImg.src = "images/"+current[0]+current[1]+".png";
+	caption.textContent = current[0].toUpperCase() +' ('+current[1]+'/'+exercises+')'
 }
 
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
+document.getElementById("close-button").onclick = function() {
+    document.body.style.overflow = 'auto';
 	modal.style.display = "none";
 }
+
